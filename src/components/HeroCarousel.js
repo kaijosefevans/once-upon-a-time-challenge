07d8f17ff3e50463badce52af79008be351f7c53
Kai <kaijosefevans@gmail.com>
2021-09-03 03:04:00 -0700

    fixed styling for text hovering over carousel

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -20,29 +20,42 @@ function HeroCarousel(props) {
       // else, return string in original state
       : props.items.hero_text.title;
 
+      // declare var options for options of display for date
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      // declare var date of props.items.hero_text.date transposed into proper title format
+      const date = new Date(props.items.hero_text.date).toLocaleDateString('en-US', options);
+
     // declare var slides with value of props.items.hero_slides mapped accessing each seperate element and return seperate divs with images for carousel
     const slides = props.items.hero_slides.map((el, index) => {
       return (
         <div key={'slide' + index} className='slide-imgs'>
+          <div className='carousel-text'>
+            <p id='carousel-title1'>{title1}</p>
+            <p id='carousel-title2'>{title2}</p>
+            <p id='carousel-title-date'>{date}</p>
+            <p id='carousel-subtitle'>{props.items.hero_text.subtitle}</p>
+          </div>
           <img src={el.image} alt={el.alt}></img>
         </div>
       );
     });
 
-    // declare var options for options of display for date
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    // declare var date of props.items.hero_text.date transposed into proper title format
-    const date = new Date(props.items.hero_text.date).toLocaleDateString('en-US', options);
 
     return (
-      <div>
-        <div className='carousel-text'>
+      <div className="container">
+        {/* <div className='carousel-text'>
           <p id='carousel-title1'>{title1}</p>
           <p id='carousel-title2'>{title2}</p>
           <p id='carousel-title-date'>{date}</p>
           <p id='carousel-subtitle'>{props.items.hero_text.subtitle}</p>
-        </div>
+        </div> */}
         <div className="slider-container">
+        {/* <div className='carousel-text'>
+            <p id='carousel-title1'>{title1}</p>
+            <p id='carousel-title2'>{title2}</p>
+            <p id='carousel-title-date'>{date}</p>
+            <p id='carousel-subtitle'>{props.items.hero_text.subtitle}</p>
+          </div> */}
           <Carousel infiniteLoop autoPlay interval={5000} showThumbs={false} showStatus={false} showArrows={false} dynamicHeight={true}>
             {slides}
           </Carousel>
